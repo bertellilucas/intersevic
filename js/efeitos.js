d8fdb886ec830b0318c1888c50f33566a510e4ee
lbertelli <lbertelli29@gmail.com>
2018-06-08 15:54:57 -0300

    Initial commit

---
+++ js/efeitos.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $(".ocultando").toggle();
+      });
+
+    $("#show").click(function(){
+      $(".aparecendo").toggle();
+    });
+
+});
